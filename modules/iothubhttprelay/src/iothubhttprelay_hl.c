dd7b5ecfd6499808b43ea651a27faa18dae72055
Yawei Wang <yaweiw@microsoft.com>
2016-09-04 21:07:40 +0800

    x

--- modules/iothubhttprelay/src/iothubhttprelay_hl.c
+++ modules/iothubhttprelay/src/iothubhttprelay_hl.c
@@ -12,10 +12,12 @@
 #include "azure_c_shared_utility/iot_logging.h"
 #include "parson.h"
 
-#define SUFFIX "IoTHubSuffix"
-#define HUBNAME "IoTHubName"
+#define LIGHT light
+#define VIBRANT vibrant
 
-static MODULE_HANDLE IoTHubHttp_HL_Create(MESSAGE_BUS_HANDLE busHandle, const void* configuration)
+#define DEVICECONNECTIONSTRING DeviceConnectionString
+
+static MODULE_HANDLE IoTHubHttpRelay_HL_Create(MESSAGE_BUS_HANDLE busHandle, const void* configuration)
 {
     MODULE_HANDLE *result;
     if ((busHandle == NULL) || (configuration == NULL))
@@ -46,29 +48,19 @@ static MODULE_HANDLE IoTHubHttp_HL_Create(MESSAGE_BUS_HANDLE busHandle, const vo
             }
             else
             {
-                const char * IoTHubName;
-                const char * IoTHubSuffix;
-                if ((IoTHubName = json_object_get_string(obj, HUBNAME)) == NULL)
+                const char * DeviceConnectionString;
+                if ((DeviceConnectionString = json_object_get_string(obj, DEVICECONNECTIONSTRING)) == NULL)
                 {
                     /*Codes_SRS_IOTHUBHTTP_HL_17_006: [ If the JSON object does not contain a value named "IoTHubName" then IoTHubHttp_HL_Create shall fail and return NULL. ]*/
-                    LogError("Did not find expected %s configuration", HUBNAME);
-                    result = NULL;
-                }
-                else if ((IoTHubSuffix = json_object_get_string(obj, SUFFIX)) == NULL)
-                {
-                    /*Codes_SRS_IOTHUBHTTP_HL_17_007: [ If the JSON object does not contain a value named "IoTHubSuffix" then IoTHubHttp_HL_Create shall fail and return NULL. ]*/
-                    LogError("Did not find expected %s configuration", SUFFIX);
+                    LogError("Did not find expected %s configuration", DEVICECONNECTIONSTRING);
                     result = NULL;
                 }
                 else
                 {
                     /*Codes_SRS_IOTHUBHTTP_HL_17_008: [ IoTHubHttp_HL_Create shall invoke iothubhttp Module's create, using the busHandle, IotHubName, and IoTHubSuffix. ]*/
-                    IOTHUBHTTP_CONFIG llConfiguration;
-                    llConfiguration.IoTHubName = IoTHubName;
-                    llConfiguration.IoTHubSuffix = IoTHubSuffix;
-                    /*Codes_SRS_IOTHUBHTTP_HL_17_009: [ When the lower layer IoTHubHttp create succeeds, IoTHubHttp_HL_Create shall succeed and return a non-NULL value. ]*/
-                    /*Codes_SRS_IOTHUBHTTP_HL_17_010: [ If the lower layer IoTHubHttp create fails, IoTHubHttp_HL_Create shall fail and return NULL. ]*/
-                    result = MODULE_STATIC_GETAPIS(IOTHUBHTTP_MODULE)()->Module_Create(busHandle, &llConfiguration);
+                    IOTHUBHTTPRELAY_CONFIG llConfiguration;
+                    llConfiguration.DeviceConnectionString = DeviceConnectionString;
+                    result = MODULE_STATIC_GETAPIS(IOTHUBHTTPRELAY_MODULE)()->Module_Create(busHandle, &llConfiguration);
                 }
             }
             json_value_free(json);
@@ -77,34 +69,34 @@ static MODULE_HANDLE IoTHubHttp_HL_Create(MESSAGE_BUS_HANDLE busHandle, const vo
     return result;
 }
 
-static void IoTHubHttp_HL_Destroy(MODULE_HANDLE moduleHandle)
+static void IoTHubHttpRelay_HL_Destroy(MODULE_HANDLE moduleHandle)
 {
     /*Codes_SRS_IOTHUBHTTP_HL_17_012: [ IoTHubHttp_HL_Destroy shall free all used resources. ]*/
-    MODULE_STATIC_GETAPIS(IOTHUBHTTP_MODULE)()->Module_Destroy(moduleHandle);
+    MODULE_STATIC_GETAPIS(IOTHUBHTTPRELAY_MODULE)()->Module_Destroy(moduleHandle);
 }
 
 
 static void IoTHubHttp_HL_Receive(MODULE_HANDLE moduleHandle, MESSAGE_HANDLE messageHandle)
 {
     /*Codes_SRS_IOTHUBHTTP_HL_17_011: [ IoTHubHttp_HL_Receive shall pass the received parameters to the underlying IoTHubHttp receive function. ]*/
-    MODULE_STATIC_GETAPIS(IOTHUBHTTP_MODULE)()->Module_Receive(moduleHandle, messageHandle);
+    MODULE_STATIC_GETAPIS(IOTHUBHTTPRELAY_MODULE)()->Module_Receive(moduleHandle, messageHandle);
 }
 
 /*Codes_SRS_IOTHUBHTTP_HL_17_014: [ The MODULE_APIS structure shall have non-NULL Module_Create, Module_Destroy, and Module_Receive fields. ]*/
-static const MODULE_APIS IoTHubHttp_HL_GetAPIS_Impl =
+static const MODULE_APIS IoTHubHttpRelay_HL_GetAPIS_Impl =
 {
-    IoTHubHttp_HL_Create,
-    IoTHubHttp_HL_Destroy,
-    IoTHubHttp_HL_Receive
+    IoTHubHttpRelay_HL_Create,
+    IoTHubHttpRelay_HL_Destroy,
+    IoTHubHttpRelay_HL_Receive
 };
 
 
 #ifdef BUILD_MODULE_TYPE_STATIC
-MODULE_EXPORT const MODULE_APIS* MODULE_STATIC_GETAPIS(IOTHUBHTTP_MODULE_HL)(void)
+MODULE_EXPORT const MODULE_APIS* MODULE_STATIC_GETAPIS(IOTHUBHTTPRELAY_MODULE_HL)(void)
 #else
 MODULE_EXPORT const MODULE_APIS* Module_GetAPIS(void)
 #endif
 {
     /*Codes_SRS_IOTHUBHTTP_HL_17_013: [ Module_GetAPIS shall return a non-NULL pointer. ]*/
-    return &IoTHubHttp_HL_GetAPIS_Impl;
+    return &IoTHubHttpRelay_HL_GetAPIS_Impl;
 }

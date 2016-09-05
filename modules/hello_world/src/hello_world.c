9b7b85567cd81caaa59c3ff622e3fac862e6b0ad
Yawei Wang <yaweiw@microsoft.com>
2016-09-05 15:18:59 +0800

    xx

--- modules/hello_world/src/hello_world.c
+++ modules/hello_world/src/hello_world.c
@@ -36,13 +36,16 @@ void Print(const char* msg) {
 
 int helloWorldThread(void *param)
 {
+    Print("in helloWorldThread");
     MMAPDATA_HANDLE p_mmapData; // here our mmapped data will be accessed
     int fd_mmapFile; // file descriptor for memory mapped file
     /* Create shared memory object and set its size */
     fd_mmapFile = open(mmapFilePath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    Print("after open");
     if (fd_mmapFile == -1) exitError("fd error; check errno for details");
     /* Map shared memory object read-writable */
     p_mmapData = (MMAPDATA_HANDLE)(mmap(NULL, sizeof(MMAPDATA), PROT_READ | PROT_WRITE, MAP_SHARED, fd_mmapFile, 0));
+    Print("after mmap");
     if (p_mmapData == MAP_FAILED) exitError("mmap error");
 
     HELLOWORLD_HANDLE_DATA* handleData = param;
@@ -55,13 +58,28 @@ int helloWorldThread(void *param)
     }
     else
     {
+        Print("before while(1)");
         while (1)
         {
             if (pthread_mutex_lock(&(p_mmapData->mutex)) != 0) exitError("pthread_mutex_lock");
             if (pthread_cond_wait(&(p_mmapData->cond), &(p_mmapData->mutex)) != 0) exitError("pthread_cond_wait");
             // signal to waiting thread
-            printf("p_mmapData->light = %d\r\n", p_mmapData->light);
-            printf("p_mmapData->vibrant = %d\r\n", p_mmapData->vibrant);
+            if(p_mmapData->light != NULL)
+            {
+                printf("p_mmapData->light = %d\r\n", p_mmapData->light);
+            }
+            else
+            {
+                printf("p_mmapData->light is NULL\r\n");
+            }
+            if(p_mmapData->vibrant != NULL)
+            {
+                printf("p_mmapData->vibrant = %d\r\n", p_mmapData->vibrant);
+            }
+            else
+            {
+                printf("p_mmapData->vibrant is NULL\r\n");
+            }
             if (pthread_mutex_unlock(&(p_mmapData->mutex)) != 0) exitError("pthread_mutex_unlock");
 
             char lightVal[10];
@@ -105,10 +123,11 @@ int helloWorldThread(void *param)
                 {
                     /*shall retry*/
                 }
-                    (void)ThreadAPI_Sleep(5000); /*every 5 seconds*/
+                (void)ThreadAPI_Sleep(5000); /*every 5 seconds*/
             }
                 Message_Destroy(helloWorldMessage);
         }
+        Print("after while(1)");
     }
     return 0;
 }

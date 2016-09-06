1f823c1fc1756ab10d3c0438484b8a66579a15fd
Yawei Wang <yaweiw@microsoft.com>
2016-09-06 09:22:36 +0800

    x

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -14,11 +14,11 @@ int main(int argc, char** argv)
 
     /* Create shared memory object and set its size */
     fd_mmapFile = open(mmapFilePath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if (fd_mmapFile == -1) exitError("fd error; check errno for details");
+    if (fd_mmapFile == -1) printf("fd error; check errno for details\n");
     
     /* Map shared memory object read-writable */
     p_mmapData = (MMAPDATA_HANDLE)(mmap(NULL, sizeof(MMAPDATA), PROT_READ | PROT_WRITE, MAP_SHARED, fd_mmapFile, 0));
-    if (p_mmapData == MAP_FAILED) exitError("mmap error");
+    if (p_mmapData == MAP_FAILED) printf("mmap error\n");
 
     GATEWAY_HANDLE gateway;
     if (argc != 2)
@@ -37,15 +37,15 @@ int main(int argc, char** argv)
             printf("gateway successfully created from JSON\n");
             printf("gateway shall run until ENTER is pressed\n");
             //(void)getchar();
-            
+
             while(getchar() != 'e')
             {
-                if (pthread_mutex_lock(&(p_mmapData->mutex)) != 0) exitError("pthread_mutex_lock");
-                if (pthread_cond_wait(&(p_mmapData->cond), &(p_mmapData->mutex)) != 0) exitError("pthread_cond_wait");
+                if (pthread_mutex_lock(&(p_mmapData->mutex)) != 0) printf("pthread_mutex_lock err\n");
+                if (pthread_cond_wait(&(p_mmapData->cond), &(p_mmapData->mutex)) != 0) printf("pthread_cond_wait err\n");
                 // signal to waiting thread
                 printf("p_mmapData->light = %d\r\n", p_mmapData->light);
                 printf("p_mmapData->vibrant = %d\r\n", p_mmapData->vibrant);
-                if (pthread_mutex_unlock(&(p_mmapData->mutex)) != 0) exitError("pthread_mutex_unlock");
+                if (pthread_mutex_unlock(&(p_mmapData->mutex)) != 0) printf("pthread_mutex_unlock err\n");
             }
 
             Gateway_LL_Destroy(gateway);
